f36e8ff5748080e2f1223831afe7bafba4c191c4
Jimmy <dm.a.uvarov@gmail.com>
2022-03-20 11:59:09 -0400

    commit tree 7c6f871fd7b0363014e4fa7e3792e3fe7e9a5f3c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    CLICBAIT = ["Won't Believe","Secret","Guess"]
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction","Non-Fiction"]
+    #validate  :is_clickbait #, message: {"clickbait"}
+    validates :title, exlusion: { in: ["Won't Believe","Secret","Guess"], message: "too clickbaity"}
+    
+
+    # def is_clickbait
+    #    found = CLICBAIT.filter{ |e| title == e}
+    #    if found
+    #     errors.add(:title,"clickbait")
+    #    end
+
+    # end
 end
